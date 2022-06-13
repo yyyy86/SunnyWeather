e55f1a3ada4ff2b938ca202bb246d2c0b465125a
yyyy86 <1061917728@qq.com>
2022-06-13 21:15:36 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
@@ -0,0 +1,18 @@
+package com.example.sunnyweather
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication:Application() {
+    companion object{
+        const val TOKEN = "VrEzbfo7n1N60Xyc"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
